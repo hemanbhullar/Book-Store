8a707885c83cf6456bf7adec4a84f7411019f251
hemanbhullar <102414075+hemanbhullar@users.noreply.github.com>
2023-11-30 19:25:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/books", router)// localhost
 
 
-mongoose.connect("")
+mongoose.connect(DATABASE_URL);
 .then(()=>console.log("connect hogya Bhaiyo"))
 .then(()=> {
     app.listen(5000);
